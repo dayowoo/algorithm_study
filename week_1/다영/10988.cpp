cd08e45900fb25a47ae8f55e0586473af8e3340b
dayowoo <kkbo5222@gmail.com>
2025-01-16 20:41:09 +0900

    solution 10988 - dayoung

--- week_1/다영/10988.cpp
+++ week_1/다영/10988.cpp
@@ -1,3 +1,20 @@
 /*
 답 : http://boj.kr/c7b230ec84bf4153a125b858a5ef77e2
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+string str;
+
+int main() {
+	cin >> str;
+	string a = str;
+	
+	reverse(a.begin(), a.end());
+	
+	if(str==a) cout << true;
+	else cout << false;
+	
+	return 0;
+}
