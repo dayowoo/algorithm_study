fd1120a109213fc9998d71f5fb9bded8ba33ad2d
qlghwp123 <qlghwp123@naver.com>
2025-01-18 13:09:50 +0900

    1-D 10988번 해결

--- week_1/동근/10988.cpp
+++ week_1/동근/10988.cpp
@@ -1,3 +1,23 @@
 /*
 답 : http://boj.kr/c7b230ec84bf4153a125b858a5ef77e2
 */
+/**
+ *  생각
+ *  - 단순하게 복사본을 만들어서 뒤집고 비교 !
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+  string s;
+  cin>>s;
+
+  string s1(s);
+  reverse(s1.begin(),s1.end());
+
+  int result = s.compare(s1) != 0 ? 0 : 1;
+  cout << result  << '\n';
+
+  return 0;
+}
