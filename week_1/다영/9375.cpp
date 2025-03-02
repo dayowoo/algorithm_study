1287b09d4dcc602a91fc0025851a9e94447e3ab7
dayowoo <kkbo5222@gmail.com>
2025-03-02 16:06:49 +0900

    [ADD] soultion 9375

--- week_1/다영/9375.cpp
+++ week_1/다영/9375.cpp
@@ -1,3 +1,31 @@
 /*
 답 : http://boj.kr/2c284c870fb64ae8adb0a5760111c03d
 */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    string a, b;
+    cin >> n;
+    
+    while(n--) {
+    	cin >> m;
+    	
+    	map<string, int> mp;
+    	for(int i=0; i<m; i++) {
+    		cin >> a >> b;
+    		mp[b] ++;
+		}
+		long long ret=1;
+		for(auto c: mp) {
+			ret *= ((long long)c.second +1);
+		}
+		ret--;
+		cout << ret;
+	}
+    return 0;
+}
+
+
